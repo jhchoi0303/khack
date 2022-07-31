e6b49e23463c94d8bf4a88b6c415f18d3c7ab455
jhchoi0303 <zzzmilky@naver.com>
2022-07-31 11:10:49 +0900

    FIX config

--- khack/config.php
+++ khack/config.php
@@ -1,6 +1,6 @@
 <?php
     $host = "localhost";
-    $dbid = "db2019100033";                    // 자신의 DB ID
-    $dbpass = "zzzmilky@naver.com";                  // 자신의 DB Password
-    $dbname = "db2019100033";                  // 해당 프로젝트에서 사용하는 DB명
+    $dbid = "root";                    // 자신의 DB ID
+    $dbpass = "choijihyun33!";                  // 자신의 DB Password
+    $dbname = "dbkhack";                  // 해당 프로젝트에서 사용하는 DB명
 ?>
